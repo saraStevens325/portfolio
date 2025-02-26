61ca329af70c96cba25ffa5e349969711f49daa6
saraStevens325 <Sara_Stevens_1@proton.me>
2025-02-25 16:15:08 -0800

    updated services data

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -2,32 +2,32 @@ const Services_Data = [
     {
         s_no:"01",
         s_name:"Web design",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Experience using ninga forms, figma, canva, wordpress, and elementor to design websiteas."
     },
     {
         s_no:"02",
-        s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Backend web development",
+        s_desc:"Experience with python and Flask to build backend web application functionality"
     },
     {
         s_no:"03",
-        s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Frontend web development",
+        s_desc:"Created four projects using react, javascript, HTML, and CSS."
     },
     {
         s_no:"04",
-        s_name:"App design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Database design",
+        s_desc:"Designed an implemented databases using SQL, mySQL, PostgreSQL, and SQLAlchemy."
     },
     {
         s_no:"05",
-        s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"API development",
+        s_desc:"Implemented RESTful API's in two projects using javascript to successfully access database information."
     },
     {
         s_no:"06",
-        s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"AWS cloud services",
+        s_desc:"Experience with AWS cloud services including EC2, Route53, and s3."
     },
 ]
  
